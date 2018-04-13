1fb2c0dcad8f0f807826ff610415bb71757530e4
Thomas Röggla <troeggla@users.noreply.github.com>
2018-04-13 19:17:27 +0200

    chore(typings): Improve typings to match new state of library
    * exporting StreamDeck type as a class
    
    * added function clearAllKeys() to type definition file
    
    * added typings.d.ts to 'files', to include it in the npm package
    
    * added interface for sharp constructor options
    
    * added type definition for the function fillPanel()
    
    * added type definitions for all visible static functions

--- typings.d.ts
+++ typings.d.ts
@@ -1,14 +1,39 @@
-interface IStreamDeck {
+interface SharpOptions {
+  failOnError?: boolean;
+  density?: number;
+  raw?: {
+    width?: number,
+    height?: number,
+    channels?: number,
+  };
+  create?: {
+    width?: number,
+    height?: number,
+    channels?: number,
+    background?: string | any;
+  };
+}
+
+declare class StreamDeck {
 	readonly ICON_SIZE: number;
+
+	static checkRGBValue(value: number): void;
+	static checkValidKeyIndex(keyIndex: number): void;
+	static bufferToIntArray(buffer: Buffer): Array<number>;
+
+	constructor(devicePath?: string);
 	write(buffer: Buffer): void;
 	sendFeatureReport(buffer: Buffer): void;
 
 	setBrightness(percentage: number): void;
 
 	clearKey(keyIndex: number): void;
+	clearAllKeys(): void;
+
 	fillColor(keyIndex: number, r: number, g: number, b: number): void;
 	fillImage(keyIndex: number, imageBuffer: Buffer): void;
 	fillImageFromFile(keyIndex: number, filePath: string): Promise<void>;
+	fillPanel(imagePathOrBuffer: string | Buffer, sharpOptions: SharpOptions): Promise<void>;
 
 	bufferToIntArray(buffer: Buffer): number[];
 
@@ -18,5 +43,4 @@ interface IStreamDeck {
 	on(event: 'error', handler: (error: Error) => void): this;
 }
 
-const streamDeck: IStreamDeck;
-export = streamDeck;
+export = StreamDeck;
