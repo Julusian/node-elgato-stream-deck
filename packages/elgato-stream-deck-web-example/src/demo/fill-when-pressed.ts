556ff7961d39d943437131778243a6b942d28b9f
Julian Waller <git@julusian.co.uk>
2019-09-11 22:27:43 +0100

    fix: broken demo

--- packages/elgato-stream-deck-web-example/src/demo/fill-when-pressed.ts
+++ packages/elgato-stream-deck-web-example/src/demo/fill-when-pressed.ts
@@ -1,7 +1,6 @@
 import { Demo } from './demo'
 import { StreamDeck } from 'elgato-stream-deck-web'
 
-// TODO - sticking keys..
 export class FillWhenPressedDemo implements Demo {
 	private pressed: number[] = []
 
@@ -21,7 +20,7 @@ export class FillWhenPressedDemo implements Demo {
 	async keyUp(device: StreamDeck, keyIndex: number): Promise<void> {
 		const index = this.pressed.indexOf(keyIndex)
 		if (index !== -1) {
-			this.pressed = this.pressed.splice(index, 1)
+			this.pressed.splice(index, 1)
 
 			await device.clearKey(keyIndex)
 		}
