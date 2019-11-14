c9357acefa85df5870288212e7d31d01b4b68688
Julian Waller <git@julusian.co.uk>
2019-11-14 10:12:13 +0000

    feat: refactor originalv2 and xl to a shared base class

---
+++ src/models/base-gen2.ts
@@ -0,0 +1,94 @@
+import { encodeJPEG } from '../jpeg'
+import { imageToByteArray, numberArrayToString } from '../util'
+import { StreamDeckBase } from './base'
+import { KeyIndex } from './id'
+
+/**
+ * Base class for generation 2 hardware (starting with the xl)
+ */
+export abstract class StreamDeckGen2Base extends StreamDeckBase {
+	/**
+	 * Sets the brightness of the keys on the Stream Deck
+	 *
+	 * @param {number} percentage The percentage brightness
+	 */
+	public setBrightness(percentage: number) {
+		if (percentage < 0 || percentage > 100) {
+			throw new RangeError('Expected brightness percentage to be between 0 and 100')
+		}
+
+		// prettier-ignore
+		const brightnessCommandBuffer = [
+			0x03, 0x08, percentage, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+		]
+		this.device.sendFeatureReport(brightnessCommandBuffer)
+	}
+
+	public resetToLogo() {
+		// prettier-ignore
+		const resetCommandBuffer = [
+			0x03,
+			0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+		]
+		this.device.sendFeatureReport(resetCommandBuffer)
+	}
+
+	public getFirmwareVersion() {
+		return numberArrayToString(this.device.getFeatureReport(5, 32).slice(6))
+	}
+
+	public getSerialNumber() {
+		return numberArrayToString(this.device.getFeatureReport(6, 32).slice(2))
+	}
+
+	protected transformKeyIndex(keyIndex: KeyIndex): KeyIndex {
+		return keyIndex
+	}
+
+	protected getFillImageCommandHeaderLength() {
+		return 8
+	}
+
+	protected writeFillImageCommandHeader(
+		buffer: Buffer,
+		keyIndex: number,
+		partIndex: number,
+		isLast: boolean,
+		bodyLength: number
+	) {
+		buffer.writeUInt8(0x02, 0)
+		buffer.writeUInt8(0x07, 1)
+		buffer.writeUInt8(keyIndex, 2)
+		buffer.writeUInt8(isLast ? 1 : 0, 3)
+		buffer.writeUInt16LE(bodyLength, 4)
+		buffer.writeUInt16LE(partIndex++, 6)
+	}
+
+	protected getFillImagePacketLength() {
+		return 1024
+	}
+
+	protected convertFillImage(sourceBuffer: Buffer, sourceOffset: number, sourceStride: number): Buffer {
+		const byteBuffer = imageToByteArray(
+			sourceBuffer,
+			sourceOffset,
+			sourceStride,
+			0,
+			this.transformCoordinates.bind(this),
+			'rgba',
+			this.ICON_SIZE
+		)
+
+		return encodeJPEG(byteBuffer, this.ICON_SIZE, this.ICON_SIZE)
+	}
+
+	private transformCoordinates(x: number, y: number): { x: number; y: number } {
+		return { x: this.ICON_SIZE - x - 1, y: this.ICON_SIZE - y - 1 }
+	}
+}
--- src/models/originalv2.ts
+++ src/models/originalv2.ts
@@ -1,9 +1,8 @@
-import { encodeJPEG } from '../jpeg'
-import { imageToByteArray, numberArrayToString } from '../util'
-import { OpenStreamDeckOptions, StreamDeckBase, StreamDeckProperties } from './base'
-import { DeviceModelId, KeyIndex, StreamDeckDeviceInfo } from './id'
+import { OpenStreamDeckOptions, StreamDeckProperties } from './base'
+import { StreamDeckGen2Base } from './base-gen2'
+import { DeviceModelId, StreamDeckDeviceInfo } from './id'
 
-const xlProperties: StreamDeckProperties = {
+const origV2Properties: StreamDeckProperties = {
 	MODEL: DeviceModelId.ORIGINALV2,
 	COLUMNS: 5,
 	ROWS: 3,
@@ -12,92 +11,8 @@ const xlProperties: StreamDeckProperties = {
 	KEY_DATA_OFFSET: 4
 }
 
-export class StreamDeckOriginalV2 extends StreamDeckBase {
+export class StreamDeckOriginalV2 extends StreamDeckGen2Base {
 	constructor(deviceInfo: StreamDeckDeviceInfo, options: OpenStreamDeckOptions) {
-		super(deviceInfo, options, xlProperties)
-	}
-	/**
-	 * Sets the brightness of the keys on the Stream Deck
-	 *
-	 * @param {number} percentage The percentage brightness
-	 */
-	public setBrightness(percentage: number) {
-		if (percentage < 0 || percentage > 100) {
-			throw new RangeError('Expected brightness percentage to be between 0 and 100')
-		}
-
-		// prettier-ignore
-		const brightnessCommandBuffer = [
-			0x03, 0x08, percentage, 0x00, 0x00, 0x00, 0x00, 0x00,
-			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-		]
-		this.device.sendFeatureReport(brightnessCommandBuffer)
-	}
-
-	public resetToLogo() {
-		// prettier-ignore
-		const resetCommandBuffer = [
-			0x03,
-			0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-		]
-		this.device.sendFeatureReport(resetCommandBuffer)
-	}
-
-	public getFirmwareVersion() {
-		return numberArrayToString(this.device.getFeatureReport(5, 32).slice(6))
-	}
-
-	public getSerialNumber() {
-		return numberArrayToString(this.device.getFeatureReport(6, 32).slice(2))
-	}
-
-	protected transformKeyIndex(keyIndex: KeyIndex): KeyIndex {
-		return keyIndex
-	}
-
-	protected getFillImageCommandHeaderLength() {
-		return 8
-	}
-
-	protected writeFillImageCommandHeader(
-		buffer: Buffer,
-		keyIndex: number,
-		partIndex: number,
-		isLast: boolean,
-		bodyLength: number
-	) {
-		buffer.writeUInt8(0x02, 0)
-		buffer.writeUInt8(0x07, 1)
-		buffer.writeUInt8(keyIndex, 2)
-		buffer.writeUInt8(isLast ? 1 : 0, 3)
-		buffer.writeUInt16LE(bodyLength, 4)
-		buffer.writeUInt16LE(partIndex++, 6)
-	}
-
-	protected getFillImagePacketLength() {
-		return 1024
-	}
-
-	protected convertFillImage(sourceBuffer: Buffer, sourceOffset: number, sourceStride: number): Buffer {
-		const byteBuffer = imageToByteArray(
-			sourceBuffer,
-			sourceOffset,
-			sourceStride,
-			0,
-			this.transformCoordinates.bind(this),
-			'rgba',
-			this.ICON_SIZE
-		)
-
-		return encodeJPEG(byteBuffer, this.ICON_SIZE, this.ICON_SIZE)
-	}
-
-	private transformCoordinates(x: number, y: number): { x: number; y: number } {
-		return { x: this.ICON_SIZE - x - 1, y: this.ICON_SIZE - y - 1 }
+		super(deviceInfo, options, origV2Properties)
 	}
 }
