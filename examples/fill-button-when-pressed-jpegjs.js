5308d70813429f68f5bab8e6f4d264908f4533c4
Julian Waller <git@julusian.co.uk>
2020-03-18 18:09:30 +0000

    chore: add jpeg-js example

---
+++ examples/fill-button-when-pressed-jpegjs.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const fs = require('fs')
+const jpegJS = require('jpeg-js')
+const { openStreamDeck } = require('../dist/index')
+
+;(async () => {
+	const streamDeck = openStreamDeck()
+	streamDeck.clearAllKeys()
+
+	const rawFile = fs.readFileSync(path.resolve(__dirname, `fixtures/github_logo_${streamDeck.ICON_SIZE}.jpg`))
+	const img = jpegJS.decode(rawFile).data
+
+	streamDeck.on('down', keyIndex => {
+		// Fill the pressed key with an image of the GitHub logo.
+		console.log('Filling button #%d', keyIndex)
+		streamDeck.fillImage(keyIndex, img)
+	})
+
+	streamDeck.on('up', keyIndex => {
+		// Clear the key when it is released.
+		console.log('Clearing button #%d', keyIndex)
+		streamDeck.clearKey(keyIndex)
+	})
+
+	streamDeck.on('error', error => {
+		console.error(error)
+	})
+})()
