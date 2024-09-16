2a67996b34a135fc08fc2f3a552504f36dc55988
Julusian <Julusian@users.noreply.github.com>
2024-09-16 12:28:49 +0000

    deploy: 9402adcbd54885db63f9d51f2695894dc92b2168

--- app.js
+++ app.js
@@ -5037,7 +5037,7 @@ const chase_1 = __webpack_require__(1889);
 if (true) {
     const elm = document.querySelector('#version_str');
     if (elm) {
-        elm.innerHTML = `v${"7.0.1"}`;
+        elm.innerHTML = `v${"7.0.2"}`;
     }
 }
 function appendLog(str) {
