017533e035385cba7e33ff39d654771d7e540755
Julian Waller <git@julusian.co.uk>
2025-05-30 23:32:26 +0100

    chore: tool to generate udev rules

---
+++ scripts/regenerate-udev.mjs
@@ -0,0 +1,31 @@
+/* eslint-disable n/no-extraneous-import */
+// @ts-check
+
+import fs from 'node:fs/promises'
+
+// eslint-disable-next-line n/no-missing-import
+import { UdevRuleGenerator } from '../../udev-generator/dist/main.js'
+import { DEVICE_MODELS2, VENDOR_ID } from '@elgato-stream-deck/core'
+
+const generator = new UdevRuleGenerator()
+
+for (const model of Object.values(DEVICE_MODELS2)) {
+	for (const productId of model.productIds) {
+		if (productId >= 0xfff0) continue // Skip reserved product IDs
+		generator.addDevice(VENDOR_ID, productId)
+	}
+}
+
+const headlessStr = generator.generateFile({
+	mode: 'headless',
+})
+const desktopStr = generator.generateFile({
+	mode: 'desktop',
+})
+
+await fs.writeFile(new URL('../packages/node/udev/50-elgato-stream-deck-user.rules', import.meta.url), desktopStr)
+await fs.writeFile(new URL('../packages/node/udev/50-elgato-stream-deck-headless.rules', import.meta.url), headlessStr)
+
+await fs.writeFile(new URL('../packages/webhid/udev/50-elgato-stream-deck-user.rules', import.meta.url), desktopStr)
+
+console.log('Udev rules regenerated successfully!')
